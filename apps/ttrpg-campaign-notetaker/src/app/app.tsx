a245cfe81f8be9ec084b9d4d1e67303f4a53f9c5
Yevhen Malyi <yevhen.malyi@gmail.com>
2024-11-13 23:41:36 +0200

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/0eYq4H3QIz

---
+++ apps/ttrpg-campaign-notetaker/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="ttrpg-campaign-notetaker" />
+    </div>
+  );
+}
+
+export default App;
